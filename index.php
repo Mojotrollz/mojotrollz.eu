479bc5733c06c07055c3f2598c8404b37cfd9548
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-10-04 21:05:12 +0200

    index is no longer strict!

--- index.php
+++ index.php
@@ -9,5 +9,5 @@ require_once '/home/web/web/config/get_config.php';
 \SYSTEM\system::register_errorhandler_dbwriter();
 \SYSTEM\system::register_errorhandler_jsonoutput();
 
-echo \SYSTEM\API\api::run('\SYSTEM\API\verify', 'page_mojotrollz', array_merge($_POST,$_GET), 1, true, true);
+echo \SYSTEM\API\api::run('\SYSTEM\API\verify', 'page_mojotrollz', array_merge($_POST,$_GET), 1, false, true);
 new \SYSTEM\LOG\COUNTER("Page was called sucessfully.");
