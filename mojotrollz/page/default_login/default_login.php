d0d0993e1b2f4b5f7335c27e0b1515598eac0276
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-04 05:05:45 +0100

    login works now

--- mojotrollz/page/default_login/default_login.php
+++ mojotrollz/page/default_login/default_login.php
@@ -1,6 +1,10 @@
 <?php
 class default_login extends \SYSTEM\PAGE\Page {
+    public static function js(){
+        return array(\SYSTEM\WEBPATH(new \PPAGE(),'default_login/js/default_login.js'));}
     public function html(){
-        return SYSTEM\PAGE\replace::replaceFile(\SYSTEM\SERVERPATH(new PPAGE(),'default_login/tpl/login.tpl'), \SYSTEM\PAGE\text::tag('mojotrollz'));
+        return \SYSTEM\SECURITY\Security::isLoggedIn() ? 
+                \SYSTEM\PAGE\replace::replaceFile(\SYSTEM\SERVERPATH(new PPAGE(),'default_login/tpl/loggedin.tpl'), \SYSTEM\PAGE\text::tag('mojotrollz'))
+                : \SYSTEM\PAGE\replace::replaceFile(\SYSTEM\SERVERPATH(new PPAGE(),'default_login/tpl/loggedout.tpl'), \SYSTEM\PAGE\text::tag('mojotrollz'));
     }
 }
---
+++ mojotrollz/page/default_login/tpl/loggedin.tpl
@@ -0,0 +1,10 @@
+<div class="mojotrollz_col_content">
+    <h2>You are logged in</h2>
+    <form class="textbox" style="padding:10px" id="logout_form">
+        <div class="control-group">
+            <div class="help-block"></div>
+            <input type="hidden" />
+            <button class="btn-sm btn btn-primary" style="width: 250px;" type="submit" id="logout_submit"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> ${basic_logout}</button>
+        </div>
+    </form>
+</div>
