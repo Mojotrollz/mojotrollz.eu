92c72e8c3d61b7f4a6c7c9ae953fd709bac010ee
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-08 16:59:10 +0100

    missing sql install link

--- mojotrollz/sai/saimod_mojotrollz_npc_vendor_template/sql/DATA_SAIMOD_MOJOTROLLZ_NPC_VENDOR_TEMPLATE.php
+++ mojotrollz/sai/saimod_mojotrollz_npc_vendor_template/sql/DATA_SAIMOD_MOJOTROLLZ_NPC_VENDOR_TEMPLATE.php
@@ -3,7 +3,8 @@ namespace SQL;
 class DATA_SAIMOD_MOJOTROLLZ_NPC_VENDOR_TEMPLATE extends \SYSTEM\DB\QI {
     public static function get_class(){return \get_class();}
     public static function files_mysql(){
-        return array(   \SYSTEM\SERVERPATH(new \PSAI(),'/saimod_mojotrollz_npc_vendor_template/sql/mysql/system_page.sql'),
+        return array(   \SYSTEM\SERVERPATH(new \PSAI(),'/saimod_mojotrollz_npc_vendor_template/sql/mysql/schema_mojotrollz_npc_vendor_template.sql'),
+                        \SYSTEM\SERVERPATH(new \PSAI(),'/saimod_mojotrollz_npc_vendor_template/sql/mysql/system_page.sql'),
                         \SYSTEM\SERVERPATH(new \PSAI(),'/saimod_mojotrollz_npc_vendor_template/sql/mysql/system_api.sql'));
     }    
 }
