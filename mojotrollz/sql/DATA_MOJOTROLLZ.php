aa1cec9fff765292a8b7e98716e2db3661ca440a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-04-04 19:14:57 +0200

    fixed sql setup paths

--- mojotrollz/sql/DATA_MOJOTROLLZ.php
+++ mojotrollz/sql/DATA_MOJOTROLLZ.php
@@ -3,12 +3,12 @@ namespace SQL;
 class DATA_MOJOTROLLZ extends \SYSTEM\DB\QI {
     public static function get_class(){return \get_class();}
     public static function files_mysql(){
-        return array(   \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_page.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_api.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_text.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_cron.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/schema_mojotrollz_server.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/schema_mojotrollz_vote.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/schema_mojotrollz_addon.sql'));
+        return array(   (new \PSQL('/mysql/system_page.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/system_api.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/system_text.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/system_cron.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/schema_mojotrollz_server.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/schema_mojotrollz_vote.sql'))->SERVERPATH(),
+                        (new \PSQL('/mysql/schema_mojotrollz_addon.sql'))->SERVERPATH());
     }    
 }
