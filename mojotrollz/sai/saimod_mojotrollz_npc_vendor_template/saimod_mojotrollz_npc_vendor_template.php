895eb66e9c61152707482919932ac6b2a72ed5df
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-04-11 20:22:28 +0200

    lib handlebars reference, lib wow_tooltips reference, removed unslider reference, fixed includes of those

--- mojotrollz/sai/saimod_mojotrollz_npc_vendor_template/saimod_mojotrollz_npc_vendor_template.php
+++ mojotrollz/sai/saimod_mojotrollz_npc_vendor_template/saimod_mojotrollz_npc_vendor_template.php
@@ -135,8 +135,8 @@ class saimod_mojotrollz_npc_vendor_template extends \SYSTEM\SAI\SaiModule {
     public static function html_li_menu(){return '<li role="separator" class="nav-divider"></li><li><a data-toggle="tooltip" data-placement="left" title="test server: npc_vendor_template" href="#!mojotrollz_npc_vendor_template"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>&nbsp;&nbsp;Vendor Template</a></li>';}
     public static function right_public(){return false;}    
     public static function right_right(){return \SYSTEM\SECURITY\Security::check(\SYSTEM\SECURITY\RIGHTS::SYS_SAI);}
-    public static function js(){return array(   new \PSAI('saimod_mojotrollz_npc_vendor_template/js/handlebars-v4.0.5.js'),
-                                                new \PSAI('saimod_mojotrollz_npc_vendor_template/js/wow_tooltips.js'),
+    public static function js(){return array(   \LIB\lib_handlebars::js(),
+                                                \LIB\lib_wow_tooltips::js(),
                                                 new \PSAI('saimod_mojotrollz_npc_vendor_template/js/saimod_mojotrollz_npc_vendor_template.js'));}        
     //public static function css(){return array();}
     public static function scss(){
