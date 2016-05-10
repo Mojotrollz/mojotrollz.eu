7f710948b2438de01657785aaa61ef8969ece81d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-05-10 16:23:44 +0200

    fixed typo

--- mojotrollz/sai/saimod_mojotrollz_teamspeak/js/saimod_mojotrollz_teamspeak.js
+++ mojotrollz/sai/saimod_mojotrollz_teamspeak/js/saimod_mojotrollz_teamspeak.js
@@ -1,11 +1,11 @@
 function init_saimod_mojotrollz_teamspeak(){
-    load_visualisation_mojotrollz_server('vis_ts', 'stats_ts', '#filter_vis_ts', 'Players on Teamspeak', 900,400);        
+    load_visualisation_mojotrollz_teamspeak('vis_ts', 'stats_ts', '#filter_vis_ts', 'Players on Teamspeak', 900,400);        
     $('#filter_vis_ts').on('change',function(){
-        load_visualisation_mojotrollz_server('vis_ts', 'stats_ts', '#filter_vis_ts', 'Players on Teamspeak', 900,400);
+        load_visualisation_mojotrollz_teamspeak('vis_ts', 'stats_ts', '#filter_vis_ts', 'Players on Teamspeak', 900,400);
     })
 }
 
-function load_visualisation_mojotrollz_server(div, stats, filter, name, width, height){
+function load_visualisation_mojotrollz_teamspeak(div, stats, filter, name, width, height){
     $.getJSON('./sai.php?sai_mod=.SAI.saimod_mojotrollz_teamspeak&action='+stats+'&filter='+$(filter).val(),function(json){
         if(!json || json.status != true || !json.result){         
             return;}
