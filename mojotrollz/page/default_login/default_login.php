b41402366bbf2cdaff0e0e30a6ab18f3dfb6710b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-05-17 17:55:44 +0200

    updated system reference, meta&title test for page login

--- mojotrollz/page/default_login/default_login.php
+++ mojotrollz/page/default_login/default_login.php
@@ -1,5 +1,11 @@
 <?php
 class default_login extends \SYSTEM\PAGE\Page {
+    public static function title(){
+        return 'Mojotrollz - Login';}
+    //public static function meta(){
+    //    return array('keywords' => 'abc',
+    //                 'description' => 'cde');
+    //}
     public static function js(){
         return array(new \PPAGE('default_login/js/default_login.js'));}
     public function html(){
