8055b4238565edd5eebd56fc4ac31f4a096d3156
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-12-13 16:26:01 +0100

    fixed saimod server tbc setup script

--- mojotrollz/sai/saimod_mojotrollz_server_tbc/sql/DATA_SAIMOD_MOJOTROLLZ_SERVER_TBC.php
+++ mojotrollz/sai/saimod_mojotrollz_server_tbc/sql/DATA_SAIMOD_MOJOTROLLZ_SERVER_TBC.php
@@ -3,7 +3,7 @@ namespace SQL;
 class DATA_SAIMOD_MOJOTROLLZ_SERVER_TBC extends \SYSTEM\DB\QI {
     public static function get_class(){return \get_class();}
     public static function files_mysql(){
-        return array(   (new \PSAI('/saimod_mojotrollz_server_handling/sql/mysql/system_page.sql'))->SERVERPATH(),
-                        (new \PSAI('/saimod_mojotrollz_server_handling/sql/mysql/system_api.sql'))->SERVERPATH());
+        return array(   (new \PSAI('/saimod_mojotrollz_server_tbc/sql/mysql/system_page.sql'))->SERVERPATH(),
+                        (new \PSAI('/saimod_mojotrollz_server_tbc/sql/mysql/system_api.sql'))->SERVERPATH());
     }    
 }
