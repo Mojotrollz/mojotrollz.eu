fd9db1e65cf94ceaab52022b5c882b27a5cbf8ca
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-12-14 19:29:49 +0100

    fixed calculation error for server upstatus

--- mojotrollz/sai/saimod_mojotrollz_server_tbc/sql/qq/STATS_TBC_SERVER.php
+++ mojotrollz/sai/saimod_mojotrollz_server_tbc/sql/qq/STATS_TBC_SERVER.php
@@ -4,9 +4,9 @@ class STATS_TBC_SERVER extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
     public static function mysql(){return 
 'SELECT DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP('.\SYSTEM\SQL\system_log::FIELD_TIME.') - MOD(UNIX_TIMESTAMP('.\SYSTEM\SQL\system_log::FIELD_TIME.'),?)),"%Y/%m/%d %H:%i:%s") as day,'
-    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_REALM" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' else 0 end) realm,'
-    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_WORLD" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' else 0 end) world,'
-    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_WORLD_TEST" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' else 0 end) world_test'
+    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_REALM" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' end) realm,'
+    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_WORLD" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' end) world,'
+    .'avg(case when '.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC_WORLD_TEST" then '.\SYSTEM\SQL\system_log::FIELD_CODE.' end) world_test'
 .' FROM '.\SYSTEM\SQL\system_log::NAME_MYS
 .' WHERE '.\SYSTEM\SQL\system_log::NAME_MYS.'.'.\SYSTEM\SQL\system_log::FIELD_CLASS.' LIKE "%SERVER_ONLINE_TBC%"'
 .' GROUP BY day'
