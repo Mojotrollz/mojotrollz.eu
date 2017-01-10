433a797a331b1f8d89a480ef05f2c7d96eb74168
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-01-11 00:29:51 +0100

    #292 only login/logout bot when needed

--- mojotrollz/cron/cron_server_bot_tbc_live.php
+++ mojotrollz/cron/cron_server_bot_tbc_live.php
@@ -8,9 +8,10 @@ class cron_server_bot_tbc_live implements \SYSTEM\CRON\cronjob{
         $bots = \SQL\CRON_RANDOM_BOTS::QQ();
         while($bot = $bots->next()){
             if($bot_count > 0){
-                mojotrollz_tbc_soap::bot_login($bot['account_id'], $bot['char_id']);
+                if($bot['online'] == 0 && $bot['at_login'] == 0){
+                    mojotrollz_tbc_soap::bot_login($bot['account_id'], $bot['char_id']);}
                 $bot_count -= 1;
-            } else {
+            } elseif($bot['online'] == 1){
                 mojotrollz_tbc_soap::bot_logout($bot['account_id']);
             }
         }
--- mojotrollz/cron/qq/CRON_RANDOM_BOTS.php
+++ mojotrollz/cron/qq/CRON_RANDOM_BOTS.php
@@ -3,7 +3,8 @@ namespace SQL;
 class CRON_RANDOM_BOTS extends \SYSTEM\DB\QQ {
     public static function get_class(){return \get_class();}
     public static function mysql(){return
-'SELECT account_id, char_id FROM mojotrollz_bot'.
+'SELECT account_id, char_id, online, at_login FROM mojotrollz_bot'.
+' LEFT JOIN mangos_one_chars.characters as chars ON chars.account = account_id AND chars.guid = char_id'.
 ' WHERE server_id = 0'.
 ' ORDER BY RAND();';
     }
