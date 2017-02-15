b01dfc97323547f98dd40cb135fa657ea334a00a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-02-15 15:02:42 +0100

    fixed bot count query

--- mojotrollz/cron/qq/CRON_BOT_COUNT.php
+++ mojotrollz/cron/qq/CRON_BOT_COUNT.php
@@ -3,8 +3,8 @@ namespace SQL;
 class CRON_BOT_COUNT extends \SYSTEM\DB\QQ {
     public static function get_class(){return \get_class();}
     public static function mysql(){return
-'SELECT code as bot_count FROM system_log'.
-' WHERE class="LOG\\PLAYER_COUNT_TS"'.
+'SELECT code as bot_count, class FROM system_log'.
+' WHERE class LIKE "%PLAYER_COUNT_TS"'.
 ' ORDER BY `time` DESC'.
 ' LIMIT 1';
     }
