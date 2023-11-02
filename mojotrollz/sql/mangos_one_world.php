f4a61826893dcfd2eb3cd40d4b49223e10d21234
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-02 19:57:17 +0100

    database connections

--- mojotrollz/sql/mangos_one_world.php
+++ mojotrollz/sql/mangos_one_world.php
@@ -1,8 +1,15 @@
 <?php
 namespace SQL;
-class mangos_one_world extends \SYSTEM\DB\DBInfoMYS {
-    public function  __construct() {
-        parent::__construct('mangos_one_world', 'wow', 'asdglasi6vabc7gswv3ltcwacev', '127.0.0.1');}
+class mangos_one_world {
+    public static function connection(){
+        return new \SYSTEM\DB\DBInfoMYS(
+            'mangos_one_world',
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_USER),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_PASSWORD),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_HOST),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_PORT)
+        );
+    }
 }
 
 
--- mojotrollz/sql/mangos_zero_chars.php
+++ mojotrollz/sql/mangos_zero_chars.php
@@ -1,8 +1,15 @@
 <?php
 namespace SQL;
-class mangos_zero_chars extends \SYSTEM\DB\DBInfoMYS {
-    public function  __construct() {
-        parent::__construct('mangos_zero_chars', 'wow', 'asdglasi6vabc7gswv3ltcwacev', '127.0.0.1');}
+class mangos_zero_chars {
+    public static function connection(){
+        return new \SYSTEM\DB\DBInfoMYS(
+            'mangos_zero_chars',
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_USER),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_PASSWORD),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_HOST),
+            \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_DB_PORT)
+        );
+    }
 }
 
 
