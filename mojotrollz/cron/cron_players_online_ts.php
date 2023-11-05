bedd005c653ff313449dba86130312d39fcde99d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-05 03:34:09 +0100

    fix ts cron

--- mojotrollz/cron/cron_players_online_ts.php
+++ mojotrollz/cron/cron_players_online_ts.php
@@ -2,7 +2,7 @@
 class cron_players_online_ts implements \SYSTEM\CRON\cronjob{
     public static function run(){
         \LIB\lib_ts3::php();
-        $ts3 = PlanetTeamSpeak\TeamSpeak3Framework\TeamSpeak3::factory('serverquery://'.
+        $ts3 = TeamSpeak3::factory('serverquery://'.
             \SYSTEM\CONFIG\config::get(\config_ids::TEAMSPEAK_QUERY_USER).':'.
             \SYSTEM\CONFIG\config::get(\config_ids::TEAMSPEAK_QUERY_PASSWORD).'@'.
             \SYSTEM\CONFIG\config::get(\config_ids::TEAMSPEAK_HOST).':10011/');
