5a0e649de1e9c1f8f08948c8c7419b8a33c85ead
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-11-20 01:09:57 +0100

    remove several duplications, fix typo
    fix duplicate call of system::start (included in index.inc) and correct
    spelling for successfully

--- api.php
+++ api.php
@@ -1,7 +1,5 @@
 <?php
 require_once 'index.inc';
 
-\SYSTEM\system::start(\WEBCRAFT\config(dirname(__FILE__)));
-
-echo \SYSTEM\API\api::run('\SYSTEM\API\verify','api_mojotrollz',array_merge($_POST,$_GET));
-new \SYSTEM\LOG\COUNTER("API was called sucessfully.");
+echo \SYSTEM\API\api::run(\SYSTEM\API\verify::class,api_mojotrollz::class,array_merge($_POST,$_GET));
+new \SYSTEM\LOG\COUNTER("API was called successfully.");
